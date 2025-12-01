9d3e416a7c0966c6dd50a7c7b31d003acd70c5d5
Riddhi Dattatraya Sonawane <80909703+sonawaneriddhi@users.noreply.github.com>
2025-12-01 12:41:54 +0530

    Delete IndustrialProjectDemo directory

--- IndustrialProjectDemo/Helper.c
+++
@@ -1,8 +0,0 @@
-#include "Marvellous.h"
-
-float CircleArea(float fRadius)
-{
-    float fArea = 0.0f;
-    fArea = PI * fRadius * fRadius;
-    return fArea;
-}
--- IndustrialProjectDemo/Marvellous.h
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-
-#define PI 3.14f
-
-float CircleArea(float);
