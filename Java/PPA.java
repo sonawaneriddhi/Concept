6712c09165f5c7ffea217a5fbaabf76053a72f7e
Riddhi Dattatraya Sonawane <80909703+sonawaneriddhi@users.noreply.github.com>
2025-12-04 15:09:02 +0530

    Add files via upload

---
+++ Java/PPA.java
@@ -0,0 +1,17 @@
+import Marvellous.Hello;
+import Marvellous.Demo1;
+import Marvellous.Infosystems.LB;
+
+class PPA
+{
+    public static void main(String A[])
+    {
+        Hello hObj = new Hello();
+        Demo1 dObj = new Demo1();
+        LB lObj = new LB();
+
+        hObj.Fun();
+        dObj.Gun();
+        lObj.Sun();        
+    }
+}
